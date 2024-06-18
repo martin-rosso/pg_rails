1b5bb4ebfa43ce680587e6010b9b905c6b097405
Martín Rosso <mrosso10@gmail.com>
2024-06-18 19:17:28 -0300

    Tooltips spec

--- pg_engine/spec/system/tooltips_spec.rb
+++ pg_engine/spec/system/tooltips_spec.rb
@@ -1,14 +1,34 @@
 require 'rails_helper'
 
-describe 'Alertas' do
-  before do
-    driven_by ENV['DRIVER']&.to_sym || :selenium_chrome_headless_iphone
+describe 'Tooltips' do
+  context 'en desktop' do
+    before do
+      # byebug
+      driven_by :selenium_chrome_headless
+      # Capybara.current_driver = :selenium_chrome
+    end
+
+    it 'se activan con hover' do
+      visit '/rails/view_components/alert_component/tooltips'
+      expect(page).to have_no_text('tooltip')
+      find_by_id('tooltip').hover
+      expect(page).to have_text('tooltip')
+    end
   end
 
-  it 'los toasts desaparecen' do
-    visit '/rails/view_components/alert_component/tooltips'
-    expect(page).to have_no_text('tooltip')
-    find_by_id('tooltip').hover
-    expect(page).to have_text('tooltip')
+  context 'en mobile' do
+    before do
+      # driven_by :selenium_chrome_headless_iphone
+      driven_by :selenium_chrome_iphone
+      # driven_by :selenium_chrome_headless_notebook
+      # Capybara.current_driver = :selenium_chrome_headless_iphone
+    end
+
+    it 'se activan con click' do
+      visit '/rails/view_components/alert_component/tooltips'
+      expect(page).to have_no_text('tooltip')
+      find_by_id('tooltip').click
+      expect(page).to have_text('tooltip')
+    end
   end
 end
