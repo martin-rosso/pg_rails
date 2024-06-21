97e4a121f0600299d89b97aa19ebaca0ec34dc71
Martín Rosso <mrosso10@gmail.com>
2024-06-21 20:07:00 -0300

    ActiveStorage DirectUploads monkey patch

---
+++ pg_engine/app/overrides/activestorage_direct_uploads.rb
@@ -0,0 +1,10 @@
+# MonkeyPatch para que las direct uploads usen el service
+# local. Si se usa S3 es complicado, porque hay que setear
+# la CORS config.
+ActiveStorage::DirectUploadsController.class_eval do
+  alias_method :old_blob_args, :blob_args
+
+  def blob_args
+    old_blob_args.merge(service_name: :local)
+  end
+end
