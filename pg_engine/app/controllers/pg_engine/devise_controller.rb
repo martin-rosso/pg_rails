4841b497b3c712a3e0f5b074c14883cff69a6735
Martín Rosso <mrosso10@gmail.com>
2024-07-01 17:24:12 -0300

    Checkbox de aceptar términos y condiciones

--- pg_engine/app/controllers/pg_engine/devise_controller.rb
+++ pg_engine/app/controllers/pg_engine/devise_controller.rb
@@ -11,7 +11,7 @@ module PgEngine
     end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[nombre apellido])
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[nombre apellido accept_terms])
       devise_parameter_sanitizer.permit(:account_update, keys: %i[nombre apellido])
     end
   end
