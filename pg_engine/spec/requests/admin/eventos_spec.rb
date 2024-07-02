acbb2e30819bd2c4de8560b338126b6c1932c2f3
Martín Rosso <mrosso10@gmail.com>
2024-07-02 18:43:31 -0300

    Specs de email notifier

--- pg_engine/spec/requests/admin/eventos_spec.rb
+++ pg_engine/spec/requests/admin/eventos_spec.rb
@@ -21,12 +21,20 @@ describe 'Eventos' do
       expect(response.body).to include 'Tooltip'
       post '/a/eventos', params: {
         evento: {
-          type: 'SimpleUserNotifier',
+          type:,
           message: 'hola',
+          message_text:,
+          subject: asunto,
+          user_ids:,
           target:
         }
       }
     end
+
+    let(:asunto) { nil }
+    let(:user_ids) { nil }
+    let(:message_text) { nil }
+    let(:type) { 'SimpleUserNotifier' }
 
     context 'cuando se manda a los devs' do
       let(:target) { 'devs' }
@@ -38,6 +46,24 @@ describe 'Eventos' do
 
     context 'cuando se manda a todos' do
       let(:target) { 'todos' }
+
+      it do
+        expect { subject }.to change(Noticed::Notification, :count).by(2)
+      end
+    end
+
+    context 'cuando se manda a usuarios específicos por mail' do
+      let(:target) { 'user_ids' }
+      let(:asunto) { 'subjecttt' }
+      let(:user_ids) { 'user@host.com,otro@test.com' }
+      let(:message_text) { 'texto' }
+      let(:message) { '<h1>html</h1>' }
+      let(:type) { 'EmailUserNotifier' }
+
+      before do
+        create :user, email: 'user@host.com'
+        create :user, email: 'otro@test.com'
+      end
 
       it do
         expect { subject }.to change(Noticed::Notification, :count).by(2)
