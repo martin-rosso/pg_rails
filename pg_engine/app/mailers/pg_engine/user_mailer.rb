f3adbe8effbad026240dbd42f541a3e94b4e4328
Martín Rosso <mrosso10@gmail.com>
2024-07-03 11:28:03 -0300

    Reemplazo variables en eventos
    %{user}

--- pg_engine/app/mailers/pg_engine/user_mailer.rb
+++ pg_engine/app/mailers/pg_engine/user_mailer.rb
@@ -3,10 +3,15 @@ module PgEngine
     # default delivery_method: :smtp
 
     def notification
-      recipient = params[:notification].recipient
-      @html = params[:message].html_safe
-      @text = params[:message_text]
-      mail(to: recipient.email, subject: params[:subject])
+      @recipient = params[:notification].recipient
+      @html = replace_user(params[:message]).html_safe
+      @text = replace_user(params[:message_text])
+      mail(to: @recipient.email, subject: params[:subject])
+    end
+
+    def replace_user(input)
+      # reemplaza todas las ocurrencias de: %{user}
+      format(input, user: @recipient.nombre)
     end
   end
 end
