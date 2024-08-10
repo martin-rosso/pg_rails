91ea5090e8e4f1b1cd9f4353d23fcb93329561a0
Martín Rosso <mrosso10@gmail.com>
2024-08-10 18:20:29 -0300

    feat: allow only one tooltip instance at a time

--- pg_layout/app/javascript/config/tooltips.js
+++ pg_layout/app/javascript/config/tooltips.js
@@ -3,11 +3,21 @@ import * as bootstrap from 'bootstrap'
 document.addEventListener('turbo:load', bindTooltips)
 document.addEventListener('turbo:render', bindTooltips)
 
-function bindTooltips () {
-  const tooltipsQuery = '[data-bs-toggle="tooltip"]'
+const tooltipsQuery = '[data-bs-toggle="tooltip"]'
 
+function bindTooltips () {
   const toastElList = document.querySelectorAll(tooltipsQuery)
   Array.from(toastElList).map(tooltipEl => {
     return new bootstrap.Tooltip(tooltipEl)
   })
 }
+
+document.addEventListener('show.bs.tooltip', (ev) => {
+  const toastElList = document.querySelectorAll(tooltipsQuery)
+  Array.from(toastElList).forEach(tooltipEl => {
+    const tooltip = bootstrap.Tooltip.getInstance(tooltipEl)
+    if (tooltip) {
+      tooltip.hide()
+    }
+  })
+})
