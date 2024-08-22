313799de45dc33300f12736ead942f318d945730
Martín Rosso <mrosso10@gmail.com>
2024-08-22 18:03:04 -0300

    fix: ransack memory support for turbo frames

--- pg_engine/config/initializers/ransack_memory.rb
+++ pg_engine/config/initializers/ransack_memory.rb
@@ -1,4 +1,15 @@
 RansackMemory::Core.config = {
-  param: :q, # this means the default Ransack param name for searching. You can change it
-  session_key_format: '%controller_name%_%action_name%_%request_format%' # this means how the key used to store the information to the session will be stored. Currently it interpolates request parameters. You can customize it and use these vars to build a key that fits your needs
+  # This means the default Ransack param name for searching. You can change it
+  param: :q,
+
+  # This means how the key used to store the information to the session
+  # will be stored. Currently it interpolates request parameters.
+  # You can customize it and use these vars to build a key that fits your needs
+  #
+  # Available variables are:
+  #   controller_name
+  #   action_name
+  #   request_format
+  #   turbo_frame
+  session_key_format: '%controller_name%_%action_name%_%request_format%_%turbo_frame%'
 }
