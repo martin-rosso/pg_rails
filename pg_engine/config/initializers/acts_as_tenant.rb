86b16f76b5d9e33dbe9aaed02a02c0638b6a69ab
Martín Rosso <mrosso10@gmail.com>
2024-09-13 21:13:34 -0300

    fix: only require tenant on users controllers

--- pg_engine/config/initializers/acts_as_tenant.rb
+++ pg_engine/config/initializers/acts_as_tenant.rb
@@ -1,16 +1,7 @@
 ActsAsTenant.configure do |config|
-  config.require_tenant = true
-  # config.require_tenant = lambda do |options|
-  #   # if options[:scope] == User && true # global_domain?
-  #   #   # tal vez en algunos casos de devise sí requeriría el tenant?
-  #   #   #   creería que no, el único lugar en donde se debería queriar
-  #   #   #   User es en devise y es para obtener el current_user
-  #   #   #   hay que ver luego qué pasa con invitable
-  #   #   false
-  #   # else
-  #   #   true
-  #   # end
-  # end
+  config.require_tenant = lambda do
+    Current.namespace == :users
+  end
 
   # Customize the query for loading the tenant in background jobs
   # config.job_scope = ->{ all }
