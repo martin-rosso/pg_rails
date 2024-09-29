c1acb2299cb27f8cdb977bef0cc11eb6b3946f71
Martín Rosso <mrosso10@gmail.com>
2024-09-29 08:42:46 -0300

    fix: clear timeout with timeoutId in DOM

--- pg_layout/app/javascript/controllers/clear_timeout_controller.js
+++ pg_layout/app/javascript/controllers/clear_timeout_controller.js
@@ -3,11 +3,24 @@ import { Controller } from '@hotwired/stimulus'
 // Connects to data-controller="clear-timeout"
 export default class extends Controller {
   connect () {
-    this.element.dataset.timeoutId.split(',').forEach((el) => {
-      const timeoutId = parseInt(el)
-      clearTimeout(timeoutId)
-      console.log(`clearedTimeout: ${timeoutId}`)
-    })
+    if (this.element.dataset.timeoutId) {
+      this.element.dataset.timeoutId.split(',').forEach((el) => {
+        this.clear(el)
+      })
+    } else {
+      const headId = document.head.dataset.timeoutId
+      if (headId) {
+        headId.split(',').forEach((el) => {
+          this.clear(el)
+        })
+      }
+    }
     this.element.remove()
+  }
+
+  clear (id) {
+    const timeoutId = parseInt(id)
+    clearTimeout(timeoutId)
+    console.log(`clearedTimeout: ${timeoutId}`)
   }
 }
