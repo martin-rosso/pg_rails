09bf440a58e27de778e59a18113d448c7115b3ba
Martín Rosso <mrosso10@gmail.com>
2024-10-11 07:27:51 -0300

    style: eslint

--- pg_layout/app/javascript/application.js
+++ pg_layout/app/javascript/application.js
@@ -5,28 +5,30 @@ import './elements'
 
 import { Turbo } from '@hotwired/turbo-rails'
 
+import Trix from 'trix'
+
 document.addEventListener('trix-before-initialize', (ev) => {
-  Trix.config.lang.attachFiles = "Adjuntar archivos"
-  Trix.config.lang.bold = "Negrita"
-  Trix.config.lang.bullets = "Lista sin números"
-  Trix.config.lang.byte = "Byte"
-  Trix.config.lang.bytes = "Bytes"
-  Trix.config.lang.captionPlaceholder = "Agregá un subtítulo"
-  Trix.config.lang.code = "Monospace"
-  Trix.config.lang.heading1 = "Título"
-  Trix.config.lang.indent = "Incrementar nivel"
-  Trix.config.lang.italic = "Cursiva"
-  Trix.config.lang.link = "Linkear"
-  Trix.config.lang.numbers = "Lista numerada"
-  Trix.config.lang.outdent = "Disminuir nivel"
-  Trix.config.lang.quote = "Cita"
-  Trix.config.lang.redo = "Rehacer"
-  Trix.config.lang.remove = "Quitar"
-  Trix.config.lang.strike = "Tachar"
-  Trix.config.lang.undo = "Deshacer"
-  Trix.config.lang.unlink = "Deslinkear"
-  Trix.config.lang.url = "URL"
-  Trix.config.lang.urlPlaceholder = "Ingresá una URL"
+  Trix.config.lang.attachFiles = 'Adjuntar archivos'
+  Trix.config.lang.bold = 'Negrita'
+  Trix.config.lang.bullets = 'Lista sin números'
+  Trix.config.lang.byte = 'Byte'
+  Trix.config.lang.bytes = 'Bytes'
+  Trix.config.lang.captionPlaceholder = 'Agregá un subtítulo'
+  Trix.config.lang.code = 'Monospace'
+  Trix.config.lang.heading1 = 'Título'
+  Trix.config.lang.indent = 'Incrementar nivel'
+  Trix.config.lang.italic = 'Cursiva'
+  Trix.config.lang.link = 'Linkear'
+  Trix.config.lang.numbers = 'Lista numerada'
+  Trix.config.lang.outdent = 'Disminuir nivel'
+  Trix.config.lang.quote = 'Cita'
+  Trix.config.lang.redo = 'Rehacer'
+  Trix.config.lang.remove = 'Quitar'
+  Trix.config.lang.strike = 'Tachar'
+  Trix.config.lang.undo = 'Deshacer'
+  Trix.config.lang.unlink = 'Deslinkear'
+  Trix.config.lang.url = 'URL'
+  Trix.config.lang.urlPlaceholder = 'Ingresá una URL'
 })
 
 document.addEventListener('pg:record-created', (ev) => {
