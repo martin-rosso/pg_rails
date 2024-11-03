20190ff7e3a1f211f57e04e8b9291f6846340e10
Martín Rosso <mrosso10@gmail.com>
2024-11-03 10:52:03 -0300

    fix: migration account owners

--- pg_engine/db/migrate/20241027225618_add_membership_status_to_user_accounts.rb
+++ pg_engine/db/migrate/20241027225618_add_membership_status_to_user_accounts.rb
@@ -3,4 +3,11 @@ class AddMembershipStatusToUserAccounts < ActiveRecord::Migration[7.2]
     add_column :user_accounts, :membership_status, :integer, null: false, default: 1
     add_column :user_accounts, :invitation_status, :integer, null: false, default: 1
   end
+
+  def migrate(direction)
+    if direction == :up
+      UserAccount.update_all(profiles: [0])
+    end
+    super
+  end
 end
