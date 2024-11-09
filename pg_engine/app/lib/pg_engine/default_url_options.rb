7d63a092131a9cafc687e2d8e8f6b3c985e46a64
Martín Rosso <mrosso10@gmail.com>
2024-11-09 10:14:34 -0300

    fix: default url options, merge default action controller config

--- pg_engine/app/lib/pg_engine/default_url_options.rb
+++ pg_engine/app/lib/pg_engine/default_url_options.rb
@@ -7,12 +7,30 @@ module PgEngine
     # Aparentemente es necesario definir url_options cuando los parámetros
     # pueden cambiar a lo largo del lifecycle de la request
     def url_options
-      super.merge(PgEngine.site_brand.default_url_options).merge(tid: _current_tid)
+      base = if defined? super
+               super
+             else
+               {}
+             end
+
+      base.merge(_brand_options_with_tid)
     end
 
     # Aunque parece intuitivo que se podría definir solamente url_options,
     # es importante que default_url_options también esté definido
     def default_url_options
+      # Hay contextos en que está definido el método, como en los controllers
+      # y otros en los que no, como la Navbar
+      base = if defined? super
+               super
+             else
+               {}
+             end
+
+      base.merge(_brand_options_with_tid)
+    end
+
+    def _brand_options_with_tid
       PgEngine.site_brand.default_url_options.merge(tid: _current_tid)
     end
 
