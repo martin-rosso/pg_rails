de99bba8b5c6ac0d0e37cd4ddc90e502d9778d44
Martín Rosso <mrosso10@gmail.com>
2025-01-10 09:08:13 -0300

    fix: set z-index on flash-container
    esto es para que no interfiera con el dropdown de la navbar

--- pg_engine/app/components/flash_container_component.rb
+++ pg_engine/app/components/flash_container_component.rb
@@ -1,6 +1,8 @@
 class FlashContainerComponent < ViewComponent::Base
+  # z-1 es para que no se superponga con el dropdown de la navbar,
+  # ya que sticky-top setea un z-index de 1020 y el del dropdown es de 1000
   erb_template <<~HTML
-    <div id="flash-container" class="d-flex justify-content-around sticky-top">
+    <div id="flash-container" class="d-flex justify-content-around sticky-top z-1">
       <div id="flash" class="flash position-relative w-100 d-flex justify-content-center">
         <%= content || render(partial: 'pg_layout/flash') %>
       </div>
