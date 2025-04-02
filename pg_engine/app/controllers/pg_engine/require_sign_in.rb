65f8886742b91d020472268cd0551f687816d6ad
Martín Rosso <mrosso10@gmail.com>
2025-04-02 13:16:54 -0300

    feat: dsfil param, for dont save filters

--- pg_engine/app/controllers/pg_engine/require_sign_in.rb
+++ pg_engine/app/controllers/pg_engine/require_sign_in.rb
@@ -5,7 +5,8 @@ module PgEngine
     def self.included(clazz)
       clazz.include RansackMemory::Concern
       clazz.prepend_before_action :authenticate_user!
-      clazz.before_action :save_and_load_filters
+      # dsfil: dont save filters
+      clazz.before_action :save_and_load_filters, unless: -> { params[:dsfil] == '1' }
     end
   end
 end
