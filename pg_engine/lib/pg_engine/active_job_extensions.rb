78df785921797ddaa1dcee185519bc2575f5f08c
Martín Rosso <mrosso10@gmail.com>
2025-06-04 13:06:37 -0300

    fix: symbolize app_name on job deserialization

--- pg_engine/lib/pg_engine/active_job_extensions.rb
+++ pg_engine/lib/pg_engine/active_job_extensions.rb
@@ -8,7 +8,7 @@ module PgEngine
     end
 
     def deserialize(job_data)
-      ::Current.app_name = job_data.delete('app_name')
+      ::Current.app_name = job_data.delete('app_name')&.to_sym
       ::Current.user = User.find(job_data.delete('user_id')) if job_data['user_id'].present?
 
       super
